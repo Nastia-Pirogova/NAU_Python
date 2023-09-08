387566211a80ca7da1dd75d848f65a62adb16cb8
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-09-08 22:19:58 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# NAU_Python
+### NAU_Python
+
+# ЛАБОРАТОРНА РОБОТА №1 
+![img_1.png](img_1.png)
+
+![img.png](img.png)
--- python-lab-1/main.py
+++ python-lab-1/main.py
@@ -1,11 +1,11 @@
 # Приклад
 
 from math import cos, exp, log10, acos, sin, sqrt, atan
-
-x = float(input("Ввeдіть зачення x:"))
-
-y = ((exp(-x) - 9.3) / (log10(x) - cos(x ** 2))) ** -0.3
-print("Результат обчислення:", y)
+#
+# x = float(input("Ввeдіть зачення x:"))
+#
+# y = ((exp(-x) - 9.3) / (log10(x) - cos(x ** 2))) ** -0.3
+# print("Результат обчислення:", y)
 
 
 
@@ -21,7 +21,7 @@ print("Результат обчислення:", g)
 
 # 2 - 2 option
 
-t = float(input("Ввeдіть зачення t:"))
-
-w = ((sin(x ** 2) - cos((t - 1) ** 2) ** 4) / (atan(x + 2.6)) + sqrt(log10(t)) ** 3)
-print("Результат обчислення:", w)
+# t = float(input("Ввeдіть зачення t:"))
+#
+# w = ((sin(x ** 2) - cos((t - 1) ** 2) ** 4) / (atan(x + 2.6)) + sqrt(log10(t)) ** 3)
+# print("Результат обчислення:", w)
